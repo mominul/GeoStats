513568d871f702ea3c77e43bd4c2a66de99c754d
ripa01 <ripabiswas2739@gmail.com>
2023-11-04 03:49:53 +0600

    initial home page

--- app/views.py
+++ app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def search_view(request):
-    return render(request,'search.html')
+    return render(request,'search02.html')
+
+def home_view(request):
+    return render(request, 'index.html')
