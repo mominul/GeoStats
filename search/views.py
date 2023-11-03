dc26831f53d8ba2b969dd186629f588ca9f65ad3
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-11-03 19:19:33 +0600

    Complete search API

--- search/views.py
+++ search/views.py
@@ -4,11 +4,23 @@ from rest_framework.response import Response
 from .serializers import CitySearchSerializer
 from .models import Location
 
+def to_list(query):
+    list = []
+    for item in query:
+        list.append(item.__dict__)
+
+    return list
+
 class CitysearchView(APIView):
     def get(self, request, query):
-        
+        list = []
         matches = Location.objects.filter(city__icontains=query)[:5]
-        serializer = CitySearchSerializer(matches, many=True)
+        list += to_list(matches)
+        matches = Location.objects.filter(name__icontains=query)[:5]
+        list += to_list(matches)
+        matches = Location.objects.filter(country__icontains=query)[:5]
+        list += to_list(matches)
+        serializer = CitySearchSerializer(list, many=True)
         print(serializer.data)
         return Response(serializer.data)
 
